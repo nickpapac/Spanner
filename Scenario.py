ddaca750ab290259c29edb58c26625a4ef2b7eeb
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-07-12 16:50:05 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -20,7 +20,7 @@ def toggle_digital_output():
     print(value)
     spanner.assertTrue(value)
 
-    time.sleep(2)
+    time.sleep(3)
 
     value = testboard.digitalWrite(OUTPUT_PIN, 'LOW')
     print(value)
