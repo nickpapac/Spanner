99bea3b35a08ea80900f470f43a6eb49299d131e
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-07-16 13:15:27 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -1,53 +1,28 @@
-from Testboard import Testboard
-from Ifttt import Ifttt
+# This example will set one of our Testboard's outputs, first to HIGH, and then to LOW. 
+#
+# The goal of this example is to show you how you can drive a digital input on your device from the Testboard. 
+#
+# In our particular example, we are only setting value and not asserting anything. Of course this would never be a real world example, it's only for educational purposes
+
 import time
+from Spanner import Spanner
+from Testboard import Testboard
 
-BINARY_FROM = "SPANNER"
-TESTBOARD_ID = "200023001347343438323536"
-IFTTT_ACCESS_TOKEN = "198caec25a8b0ab77977727cb0699f48b4b0ba37"
-
+TESTBOARD_ID = "2c0019001347343438323536"
 testboard = Testboard(TESTBOARD_ID)
-ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
 
-# D7 -> Relay PIN
-RELAY_PIN = "D7"
+# Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
+OUTPUT_PIN = "D3"
 
-# Cloud Functionality
-def validate_network_cmd_on():
-    ifttt.buttonOn()
+def toggle_digital_output():
+    # set PIN state
+    value = testboard.digitalWrite(OUTPUT_PIN, 'HIGH')
+    spanner.assertTrue(value)
 
-    testboard.digitalWrite(RELAY_PIN, 'HIGH')
     time.sleep(2)
 
-    value = testboard.digitalRead(RELAY_PIN)
-    print(value)
-    if testboard.spanner_assertTrue(value) == 1:
-        return 0 #Success
-    else:
-        return 1 #Failure
-
-# Cloud Functionality
-def validate_network_cmd_off():
-    ifttt.buttonOff()
-
-    testboard.digitalWrite(RELAY_PIN, 'LOW')
-    time.sleep(2)
-
-    # check PIN state
-    value = testboard.digitalRead(RELAY_PIN)
-    print(value)
-    if (testboard.spanner_assertTrue(value) == 0):
-        return 0 # Success
-    else:
-        return 1 # Failure
+    value = testboard.digitalWrite(OUTPUT_PIN, 'LOW')
+    spanner.assertTrue(value)
 
 if __name__ == "__main__":
-
-    run_test(validate_network_cmd_on())
-
-    time.sleep(2)
-
-    #run_test(validate_network_cmd_off())
-    print(validate_network_cmd_off())
-    print('end')
-    
+    toggle_digital_output()
